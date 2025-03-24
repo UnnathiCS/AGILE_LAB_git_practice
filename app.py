c14d18a20dfe9f85ab6aa3f60bf4749c5e85a3c5
UnnathiCS <65.unnathics@gmail.com>
2025-03-24 12:46:00 +0530

    added flask app file

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("helloo from python")
+from flask import Flask
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+    return f"Hello from Python-Flask-Docker app! Timestamp: {timestamp}"
+
+if __name__ == '__main__':
+app.run(debug=True, host='0.0.0.0')
